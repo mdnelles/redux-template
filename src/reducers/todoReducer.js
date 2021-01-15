9b190afd1a963d3915ba9cf75cca5ac892441e38
mdnelles <mxnelles@gmail.com>
2021-01-15 18:34:35 -0500

    update reducer

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,36 +1,31 @@
 const initState = {
-    todoList: [
-        {
-            id: 1,
-            content: 'Play video game'
-        }, {
-            id: 2,
-            content: 'Learn nodejs & python'
-        }, {
-            id: 3,
-            content: 'Join meetup event'
-        }
-    ]
-}
+   todoList: [
+      {
+         id: 1,
+         content: "Rock Climbing",
+      },
+      {
+         id: 1,
+         content: "HIIT for 20 mins",
+      },
+   ],
+};
 
 const todoReducer = (state = initState, action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return {
-                ...state,
-                todoList: [
-                    ...state.todoList,
-                    action.payload
-                ]
-            }
-        case 'REMOVE_TODO':
-            return {
-                ...state,
-                todoList: action.payload
-            }
-        default:
-            return state
-    }
-}
+   switch (action.type) {
+      case "ADD_TODO":
+         return {
+            ...state,
+            todoList: [...state.todoList, action.payload],
+         };
+      case "REMOVE_TODO":
+         return {
+            ...state,
+            todoList: action.payload,
+         };
+      default:
+         return state;
+   }
+};
 
 export default todoReducer;
